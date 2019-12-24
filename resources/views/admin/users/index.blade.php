073fd289ff06afaff87b61055867a0f98ff2c0cb
mrakic96 <matej.rakic96@gmail.com>
2019-12-24 19:11:41 +0100

    Create edit and delete user option

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -8,9 +8,37 @@
                 <div class="card-header">Users</div>
 
                 <div class="card-body">
-                    @foreach ($users as $user)
-                        {{ $user->name }} - {{ $user->email }}
-                    @endforeach
+                    
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Roles</th>
+                            <th scope="col">Actions</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($users as $user)
+                            <tr>
+                            <th scope="row">{{ $user->id }}</th>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ implode(', ', $user->roles()->get()->pluck('name')->toArray()) }}</td>
+                                <td>
+                                <a href="{{ route('admin.users.edit', $user->id) }}"><button type="button" class="btn btn-primary float-left">Edit</button></a>
+                                <form action="{{ route('admin.users.destroy', $user) }}" method="POST" class="float-left">
+                                        @csrf
+                                        {{ method_field('DELETE') }}
+                                        <button type="submit" class="btn btn-warning">Delete</button>
+                                </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                          
+                        </tbody>
+                      </table>
                 </div>
             </div>
         </div>
