a8d45deed09f6a83c61c0a6e5e143af3d8c62583
mrakic96 <matej.rakic96@gmail.com>
2019-12-24 21:17:49 +0100

    Add alerts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,17 +55,19 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+                                    @can('manage-users')
+                                        <a class="dropdown-item" href="{{ route('admin.users.index') }}">
+                                            User Management
+                                        </a>
+                                    @endcan
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-                                @can('manage-users')
-                                <a class="dropdown-item" href="{{ route('admin.users.index') }}">
-                                    User Management
-                                </a>
-                                @endcan
-
+                                
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
@@ -78,7 +80,10 @@
         </nav>
 
         <main class="py-4">
+            <div class="container">
+            @include('partials.alerts')
             @yield('content')
+            </div>
         </main>
     </div>
 </body>
