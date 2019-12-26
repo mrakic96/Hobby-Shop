8c8d7dfea18e10ab421556967d59ffda27c6b49e
mrakic96 <matej.rakic96@gmail.com>
2019-12-26 21:22:38 +0100

    Update heigth of an uploaded image

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,7 +5,7 @@
         <div class="col">
             @foreach ($products as $product)
             <div class="card float-left" style="width: 18rem; margin: 50px 10px 10px 50px">
-                <img class="card-img-top" src="{{ url('images', $product->image) }}" alt="Card image cap">
+                <img class="card-img-top" src="{{ url('images', $product->image) }}" alt="Card image cap" style="height: 220px;">
                 <div class="card-body">
                   <h5 class="card-title">{{ $product-> name}}</h5>
                   <h6 class="card-title">Cijena: {{ $product-> price}} KM</h6>
