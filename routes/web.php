3d74702e0c567b8e2b263d2872be9ff67821ff01
mrakic96 <matej.rakic96@gmail.com>
2019-12-31 00:29:42 +0100

    Put route comments in web.php

--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,31 @@
 
 // WEB PAGES
 
+/*Main view route*/
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
+/*Shopping cart routes*/
 Route::get('/cart', 'PagesController@getCart')->middleware('can:only-logged-user-see')->name('cart');
 Route::get('/addToCart/{id}', 'PagesController@getAddToCart')->name('addToCart');
 Route::get('/add/{id}', 'PagesController@getAddByOne')->name('productAddByOne');
 Route::get('/reduce/{id}', 'PagesController@getReduceByOne')->name('productReduceByOne');
 Route::get('/remove/{id}', 'PagesController@getRemoveItem')->name('productRemove');
 
+/*Product views routes*/
 Route::get('/products', 'PagesController@products')->name('products');
 Route::get('/products/{id}', 'PagesController@product')->name('single.product');
 Route::get('/olovke', 'PagesController@olovke')->name('olovke');
 Route::get('/kistovi', 'PagesController@kistovi')->name('kistovi');
 Route::get('/platna', 'PagesController@platna')->name('platna');
 
+/*About view route*/
 Route::get('/about', 'PagesController@about')->name('about');
+
+// SEARCH
+Route::get('search', 'AutoCompleteController@index');
+ Route::get('autocomplete', 'AutoCompleteController@search');
 
 // AUTH
 
@@ -39,10 +47,12 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 //ADMIN PANEL
 
+/*Manage users routes*/
 Route::namespace('Adminpanel')->prefix('adminpanel')->name('adminpanel.')->middleware('can:manage-users')->group(function(){
     Route::resource('/users', 'UsersController', ['except' => ['show', 'create', 'store']]);
 });
 
+/*Manage products routes*/
 Route::namespace('Adminpanel')->prefix('adminpanel')->name('adminpanel.')->middleware('can:manage-products')->group(function(){
     Route::get('/products/olovke', 'ProductsController@olovke')->name('products.olovke');
     Route::get('/products/kistovi', 'ProductsController@kistovi')->name('products.kistovi');
@@ -50,6 +60,7 @@ Route::namespace('Adminpanel')->prefix('adminpanel')->name('adminpanel.')->middl
     Route::resource('/products', 'ProductsController');
 });
 
+/*Manage categories routes*/
 Route::namespace('Adminpanel')->prefix('adminpanel')->name('adminpanel.')->middleware('can:manage-products')->group(function(){
     Route::resource('/categories', 'CategoriesController');
 });
@@ -66,6 +77,3 @@ Route::namespace('Admin')->prefix('admin')->name('admin.')->middleware('can:mana
 Route::namespace('Admin')->prefix('admin')->name('admin.')->middleware('can:manage-products')->group(function(){
     Route::resource('/categories', 'CategoriesController');
 });
-// SEARCH
-Route::get('search', 'AutoCompleteController@index');
- Route::get('autocomplete', 'AutoCompleteController@search');
