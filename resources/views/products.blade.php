5901b781ce33da05d7e83a2987c50480f9a369a1
anton156 <aviskic@gmail.com>
2020-01-10 18:01:52 +0100

    Changed url path for single product

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -44,9 +44,9 @@
           @endcannot
             @foreach ($products as $product)
             <div class="card border-secondary float-left" style="width: 18rem; margin: 50px 50px 10px 0px">
-              <a href="/products/{{$product->id}}"><img class="card-img-top" src="{{ url('images', $product->image) }}" alt="Card image cap" style="height: 220px;"></a>
+              <a href="{{url('products/'.$product->id)}}"><img class="card-img-top" src="{{ url('images', $product->image) }}" alt="Card image cap" style="height: 220px;"></a>
                 <div class="card-body">
-                  <h5 class="card-title"><a class="text-primary" href="/products/{{$product->id}}">{{$product->name}}</a></h5>
+                  <h5 class="card-title"><a class="text-primary" href="{{url('products/'.$product->id)}}">{{$product->name}}</a></h5>
                   <h6 class="card-title">Cijena: {{ $product-> price}} KM</h6>
                   <hr>
                   <p class="card-text">{{ $product-> details}}</p>
