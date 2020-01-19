23c6f9bf8f166c285e3107c972230fb55433bfa2
anton156 <aviskic@gmail.com>
2020-01-19 11:19:47 +0100

    fix single product mobile

--- resources/views/view_product.blade.php
+++ resources/views/view_product.blade.php
@@ -25,7 +25,7 @@
 </ul>
 <br>
 <br>
-<div class="card border-secondary mb-3" style="top:20px; margin-bottom:136px !important; max-width: 1000px; max-height:300px">
+<div class="card border-secondary mb-3" style="top:20px; margin-bottom:136px !important; max-width: 1000px;">
     <div class="row no-gutters">
       <div class="col-md-4">
         <img src="{{ url('images', $product->image) }}" class="card-img" alt="Card image cap" style="height: 280px;">
